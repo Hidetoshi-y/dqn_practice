9895016c05aa1b1f3790fc1d5ce3f22e4bc7cfc3
yoshida Hidetoshi <hide@MacBook-Pro15.local>
2019-11-15 08:36:59 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 pip install -U pip || exit 1
+pip install numpy==1.17.4 || exit 1
+pip install gym==0.15.4 || exit 1
+pip install keras==2.3.1 || exit 1
+pip install tensorflow==1.13.1 || exit 1
+pip install keras-rl==0.4.2 || exit 1
+pip install gym[atari] || exit 1
