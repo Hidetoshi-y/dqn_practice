07a87fbf59c37c478ef691cf24ad3cd2bdbaf035
yoshida Hidetoshi <hide@MacBook-Pro15.local>
2019-11-15 11:58:51 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@ pip install -U pip || exit 1
 pip install numpy==1.17.4 || exit 1
 pip install gym==0.15.4 || exit 1
 pip install keras==2.3.1 || exit 1
-pip install tensorflow==1.13.1 || exit 1
+#pip install tensorflow==1.13.1 || exit 1
 pip install keras-rl==0.4.2 || exit 1
 pip install gym[atari] || exit 1
+
+#GPU付きでGPUが利用できるモノにはgpu用のtensorflowを利用
+if type nvidia-smi >/dev/null 2>&1 && [ x"$(nvidia-smi -L)" != x ] && [ x"$CPU" == x ]; then
+    pip install tensorflow-gpu==1.14.0 || exit 1
+else
+    pip install tensorflow==1.13.1 || exit 1
+fi
